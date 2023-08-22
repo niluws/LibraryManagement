f9157168ae1a264ab2f53804ca66160919876ad7
niluws <ns1378@yahoo.com>
2023-08-23 02:18:11 +0330

    fix CustomerBook bug

--- book/serializers.py
+++ book/serializers.py
@@ -15,7 +15,16 @@ class CustomerBookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ['title', 'category', 'stock', 'times_borrowed', 'borrowers']
+        fields = ['title', 'category', 'stock', 'times_borrowed','borrowers']
+
+    def get_times_borrowed(self, obj):
+        return obj.transaction_set.filter(transaction_type=Transaction.BOOK_BORROW).count()
+
+    def get_borrowers(self, obj):
+        borrowers = obj.transaction_set.filter(transaction_type=Transaction.BOOK_BORROW).values_list(
+            'customer__user__username', flat=True)
+        return list(borrowers)
+
 
 
 class BookSerializer(serializers.ModelSerializer):
